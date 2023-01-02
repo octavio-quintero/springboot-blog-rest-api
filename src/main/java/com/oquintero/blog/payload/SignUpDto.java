0d524332be55f2a2acdebde2ebd672c011d426a7
Octavio Quintero <octavio.quintero@outlook.com>
2023-01-02 10:47:15 -0700

    added controller to signin and signup users

---
+++ src/main/java/com/oquintero/blog/payload/SignUpDto.java
@@ -0,0 +1,11 @@
+package com.oquintero.blog.payload;
+
+import lombok.Data;
+
+@Data
+public class SignUpDto {
+    private String name;
+    private String username;
+    private String password;
+    private String email;
+}
